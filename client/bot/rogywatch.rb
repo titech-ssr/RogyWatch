48d95b3f6790dce5dac5ce1289c2c47ca2389652
Mizuho <mizuho128@gmail.com>
2016-11-22 00:34:13 +0900

    SL command

--- client/bot/rogywatch.rb
+++ client/bot/rogywatch.rb
@@ -37,7 +37,7 @@ stream.user{|status|
   case status
 
     when Twitter::Tweet then
-      puts "#{status.user.screen_name}\n#{status.text} #{(reply = status.text =~ /@#{ScreenName}/) && status.user.id == Admin} #{reply}"
+      #puts "#{status.user.screen_name}\n#{status.text} #{(reply = status.text =~ /@#{ScreenName}/) && status.user.id == Admin} #{reply}"
       content = status.text.gsub(/@#{ScreenName}/, "").strip
 
       if (reply = status.text =~ /@#{ScreenName}/) && status.user.id == Admin then
@@ -57,10 +57,18 @@ stream.user{|status|
         now = "#{(d = DateTime.now).year}_#{d.month}_#{d.day}_#{d.hour}_#{d.second}"
         Thread.new{
           begin
-            bot.connect(ws_conf[:err][:connection_timeout])
+            bot.connect(ws_conf[:std][:connection_timeout], ws_conf[:err][:connection_timeout])
 
-            if    content =~ /how/i then bot.send("HowManyPeople #{now}")
-            elsif content =~ /echo/i then bot.send("Echo #{now}")
+            if    content =~ /^how /i then bot.send("HowManyPeople #{now}")
+            elsif content =~ /^echo /i then bot.send("Echo #{content.sub(/^echo /i, "")}")
+            elsif content =~ /^fizz /i then bot.send("")
+            elsif content =~ /^isprime /i then bot.send("")
+            elsif content =~ /^sl/i then 
+              #ids = upload(["sl.gif"], rest)
+              rest.update("@#{status.user.screen_name}"[0, 140], 
+              in_reply_to_status: status,
+              media_ids: rest.upload(File.new("sl.gif")))
+              next
             else
               bot.send("HowManyPeople #{now}")
               result = bot.read(ws_conf[:timeout]).data.split(/\s+/).map{|n| n.strip}
@@ -71,6 +79,7 @@ stream.user{|status|
             end
 
             p result = bot.read(ws_conf[:timeout]).data
+            puts "result = #{result}"
             rest.update("@#{status.user.screen_name} #{result}\n#{d.to_s}"[0, 140], in_reply_to_status: status)
           rescue Exception => e
             rest.update("@#{status.user.screen_name} #{e.message}\n#{d.to_s}"[0, 140], 
