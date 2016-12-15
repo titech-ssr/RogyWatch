84235e2799f886eeadc23f903b8a4b641c54a801
mizuho <mizuho128@gmail.com>
2016-12-15 15:51:42 +0900

    check kinect connnection

--- RogyWatch/Program.cs
+++ RogyWatch/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using RogyWatchCommon;
 using APIServerModule;
+using PrimitiveServerModule;
 
 namespace RogyWatch
 {
@@ -15,6 +16,11 @@ namespace RogyWatch
             {
                 Log.logger = NLog.LogManager.GetCurrentClassLogger();
                 Log.logger.Info("RogyWatch Started");
+
+                Log.logger.Info("Connection checking...");
+                var check = PrimitiveServer.CheckConnection(10.0, 10.0);
+                if (!(check.Item1 || check.Item2)) throw new System.IO.IOException("All connection to kinect unavailable!");
+                Log.logger.Info($"Connection result V1:{check.Item1}, V2:{check.Item2}");
 
                 var core = new APIServerCore() { Config = Config.DeSerialize() };
                 Log.logger.Debug($"Config:\n{core.Config.ToString()}");
